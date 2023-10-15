5a2cc707a03e2f36bad93aae0e4e4c02ef7234af
AF1QUE <afique.dev@gmail.com>
2023-10-15 17:41:41 +0600

    Added password hashing for userModel.js file

---
+++ server/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const workspaceRoutes = require("./routes/workspaceRoutes");
+const listRoutes = require("./routes/listRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const userRoutes = require("./routes/userRoutes"); // Import the userRoutes
+
+// Create an Express application
+const app = express();
+
+// Replace '<password>' with your actual MongoDB password
+const mongoDBPassword = "3YC2Z*ZlWm6aiqqM$v";
+
+// Construct the MongoDB connection URL
+const mongoDBURL = `mongodb+srv://root:${encodeURIComponent(
+  process.env.DATABASE_PASSWORD
+)}@taskwavecluster.mgnpo9w.mongodb.net/?retryWrites=true&w=majority`;
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoDBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+// Middleware
+app.use(bodyParser.json());
+
+// Use the routes
+app.use("/api", workspaceRoutes);
+app.use("/api/workspaces/:workspaceId", listRoutes);
+app.use("/api/lists/:listId", taskRoutes);
+app.use("/api/users", userRoutes); // Include the userRoutes
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,42 +1,66 @@
 const mongoose = require("mongoose");
 
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
+
 // Define the User schema
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
+  name: {
+    type: String,
+    required: [true, "Name is required."],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    unique: [true, "Email must be unqiue"],
+    trim: true,
+    validate: [isEmail, "Please enter a valid email."],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required."],
+    minLength: [6, "Minimum password length is 6 character"],
+  },
+  workspaces: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Workspace",
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
+  ],
+  lists: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "List",
     },
-    password: {
-        type: String,
-        required: true,
+  ],
+  tasks: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Task",
     },
-    workspaces: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Workspace",
-        },
-    ],
-    lists: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "List",
-        },
-    ],
-    tasks: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Task",
-        },
-    ],
+  ],
 });
+
+// Fire a function that hashes the user password before saving the doc
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Static method to login user
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    } else throw Error("Incorrect password");
+  }
+  throw Error("Incorrect email");
+};
 
 // Create a User model
 const User = mongoose.model("User", userSchema);
